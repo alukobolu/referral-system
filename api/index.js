9577d19f2f26352a74772e23cc2182747c4ac7a4
Aluko Michael Boluwatife <alukoboluwatife3@gmail.com>
2025-08-04 08:55:50 -0700

    push

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const apiRoutes = require('../routes/api');
+const errorHandler = require('../middleware/errorHandler');
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/api', apiRoutes);
+
+// Error handler
+app.use(errorHandler);
+
+module.exports.handler = serverless(app);
